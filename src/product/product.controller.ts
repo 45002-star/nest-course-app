9d227c4704f5615b8f9804bfa0e5aed9060df11c
Boon <45002@pkw.ac.th>
2026-06-18 10:18:55 +0700

    global-helper

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,12 +1,20 @@
 import { Controller, Get } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { UtilityService } from 'src/shared/utility/utility.service';
+import { GlobalHelperService } from 'src/shared/global-helper/global-helper.service';
 
 @Controller('product')
 export class ProductController {
     constructor(private readonly productService: ProductService,
-                private readonly utilityService: UtilityService
+                private readonly utilityService: UtilityService,
+                private readonly globalHelperService: GlobalHelperService
     ) {}
+
+        @Get('/Global')
+        globalFunc(): string {
+            return this.globalHelperService.globalFunc();
+        }
+
         @Get('/shared')
         sharedFunc(): string {
             return this.utilityService.shareFunc();
