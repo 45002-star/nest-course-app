a17964be77d1426739209af19c67e578142dcb3c
Boon <45002@pkw.ac.th>
2026-06-25 11:57:20 +0700

    customer detail

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,11 @@ import { UtilityModule } from './shared/utility/utility.module';
 import { GlobalHelperModule } from './shared/global-helper/global-helper.module';
 import { CustomerModule } from './customer/customer.module';
 import { ConfigModule } from '@nestjs/config';
+import { PrismaService } from './prisma.service';
 
 @Module({
   imports: [ConfigModule.forRoot(), ProductModule, UtilityModule, GlobalHelperModule, CustomerModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
